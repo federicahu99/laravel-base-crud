b5fb5d16f530d0d6cb56b229c465a8b86be6bba2
feddericahu99 <federica.hu339@gmail.com>
2022-09-21 15:35:08 +0200

    seeder

---
+++ database/migrations/2022_09_21_132548_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 60);
+            $table->text('description');
+            $table->string('price');
+            $table->string('series');
+            $table->string('sale_date');
+            $table->string('thumb');
+            $table->enum('type', ['comic book', 'graphic novel'])->default('comic book');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
